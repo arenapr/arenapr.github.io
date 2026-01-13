a6dcefed0f3e6554efb764c11959938990da1c72
Alfredo Rodriguez <alfredo.rodriguezv@yahoo.com>
2026-01-12 22:08:04 -0700

    Added a new container that adds the WHO WE ARE part.

--- src/components/HomeNorth.js
+++ src/components/HomeNorth.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import '../App.css'
 import './HomeNorth.css'
 
@@ -10,7 +9,8 @@ function HomeNorth() {
           <img src="../images/christian-gertenbach-unsplash.jpg" alt="temp"/>
         </div>
         <div className="text">
-          <h1>ARENA</h1>
+          <h1>THE FRONT ROW<br/>BEGINS HERE</h1>
+          <h2>ARENA</h2>
         </div>
       </div>
       <div className="strip-container">
@@ -30,6 +30,16 @@ function HomeNorth() {
           <span>FASHION. CULTURE. COMMUNITY.&nbsp;</span>
           <span>FASHION. CULTURE. COMMUNITY.&nbsp;</span>
         </div>
+      </div>
+      <div className="text-center-container">
+        <h1>WHO WE ARE</h1>
+        <h2>NOT JUST A FASHION PLATFORM</h2>
+        <p>It’s a movement from the border to the world.</p>
+        <p>Born in El Paso. Built to elevate emerging designers.<br/>
+           Fueled by community, creativity and culture.<br/>
+           We’re here to spotlight what others overlook.
+        </p>
+        <p>Join the movement, share the vision and be a part of the future of fashion.</p>
       </div>
     </div>
   )
